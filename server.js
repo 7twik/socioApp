085a153f3b35101303b2070d0cae177e01ff2133
7twik <sattwik.das.ece25@heritageit.edu.in>
2022-08-26 18:11:10 +0530

    done4

--- server.js
+++ server.js
@@ -69,7 +69,7 @@ app.use("/poost", require("./routes/route"));
 if(process.env.NODE_ENV === 'production') {
   app.use(express.static('frontend/build'));
   app.get("*", (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+      res.sendFile(path.resolve(__dirname, "./frontend/build", "index.html"));
   })
 }
 
